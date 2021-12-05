7323025f7879dbb0f81a22b5495e03c0a8c2da21
Jose M Perez <jmperezvzla@gmail.com>
2021-12-04 22:53:11 -0400

    Demo: do signUp to supabase

---
+++ pages/Signup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <form @submit.prevent="doSignup">
+      <div>
+        <input type="email" placeholder="Email" />
+      </div>
+
+      <div>
+        <input type="text" placeholder="Full Name" />
+      </div>
+
+      <div>
+        <input type="text" placeholder="Username" />
+      </div>
+
+      <div>
+        <input type="password" placeholder="Password" />
+      </div>
+
+      <div>
+        <button class="btn btn-primary">Sign Up!</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { useNuxtApp } from '#app'
+
+/**
+ * @type NuxtApp
+ */
+const { $supabase } = useNuxtApp()
+
+const doSignup = async () => {
+
+  const { user, session, error } = await $supabase.auth.signUp({
+    email: 'j@gmail.com',
+    password: 'demo123'
+  })
+
+};
+</script>
