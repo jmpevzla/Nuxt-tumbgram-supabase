f5a01cb55f463d4aa1d1cd17a84ca504f55de9b8
Jose M Perez <jmperezvzla@gmail.com>
2021-12-04 23:28:16 -0400

    Add: doSignup basic with form reactive data

--- pages/Signup.vue
+++ pages/Signup.vue
@@ -2,19 +2,27 @@
   <div>
     <form @submit.prevent="doSignup">
       <div>
-        <input type="email" placeholder="Email" />
+        <input type="email"
+          placeholder="Email"
+          v-model="form.email" />
       </div>
 
       <div>
-        <input type="text" placeholder="Full Name" />
+        <input type="text"
+          placeholder="Full Name"
+          v-model="form.fullname" />
       </div>
 
       <div>
-        <input type="text" placeholder="Username" />
+        <input type="text"
+          placeholder="Username"
+          v-model="form.username" />
       </div>
 
       <div>
-        <input type="password" placeholder="Password" />
+        <input type="password"
+          placeholder="Password"
+          v-model="form.password" />
       </div>
 
       <div>
@@ -25,19 +33,37 @@
 </template>
 
 <script setup>
-import { useNuxtApp } from '#app'
+import { useNuxtApp, reactive } from '#app'
 
 /**
  * @type NuxtApp
  */
 const { $supabase } = useNuxtApp()
 
+const form = reactive({
+  email: '',
+  fullname: '',
+  username: '',
+  password: ''
+})
+
 const doSignup = async () => {
 
   const { user, session, error } = await $supabase.auth.signUp({
-    email: 'j@gmail.com',
-    password: 'demo123'
+    email: form.email,
+    password: form.password
   })
 
+  if (!error) {
+    const { data, error: pfError } = await $supabase.from('profiles').insert([
+      {
+        id: user.id,
+        username: form.username,
+        fullname: form.fullname
+      }
+    ])
+
+    console.log(data, pfError)
+  }
 };
 </script>
